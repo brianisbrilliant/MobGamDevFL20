7d2c3b1f005891ee1f9c921741d90d934899e132
Brian Foster <brianisbrilliant@gmail.com>
2020-08-26 09:24:54 -0500

    Initial Commit

---
+++ Assets/TapTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTest : MonoBehaviour
+{
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = this.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown) {
+            rend.material.color = UnityEngine.Random.ColorHSV();
+            this.transform.localScale += Vector3.one * .2f;
+        }
+    }
+}
