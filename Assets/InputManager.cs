5f2fa5610e8a0a7897e32738a49fdeea4d33cf15
Professor Foster <foster@5262-DM004.local>
2020-09-02 10:40:33 -0500

    Created new artillery scene and gameplay

---
+++ Assets/InputManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI debugText;
+    public Slider powerSlider;
+    public bool phoneIsAttached = true;
+    public Transform bulletSpawn;
+    public Rigidbody2D bullet;
+    public float minPower = 50f, maxPower = 200;
+
+    // these should be private
+    public float timer = 0;
+    public bool mouseIsDown = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        debugText.text = "Input Mgr Connected!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(phoneIsAttached) {
+            // use Input.touch
+            if(Input.touchCount > 0) {
+                Touch touch = Input.GetTouch(0);
+                debugText.text = "Pressed! Timer = " + timer;
+                timer += Time.deltaTime * 50;
+                powerSlider.value = timer;
+
+                if(touch.phase == TouchPhase.Ended) {
+                    debugText.text = "Timer = " + timer;
+                    Shoot();
+                    
+                    
+                }
+            }
+        }
+        else {
+            // use mouse0 or space or similar.
+            if(Input.GetKeyDown(KeyCode.Mouse0)) {
+                debugText.text = "You are clicking the screen!";
+                mouseIsDown = true;
+            }
+            if(Input.GetKeyUp(KeyCode.Mouse0)) {
+                mouseIsDown = false;
+                debugText.text = "Timer = " + timer;
+                Shoot();
+                
+                
+            }
+            if(mouseIsDown) {
+                timer += Time.deltaTime * 50;
+                debugText.text = "Clicked! Timer = " + timer;
+            }
+        }
+    }
+
+    void Shoot() {
+        // add a ceiling to the timer's max amount.
+        if(timer > maxPower) timer = maxPower;
+
+        debugText.text = "Pow! Power = " + timer.ToString("0.0");
+
+        Rigidbody2D rb = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+        rb.AddRelativeForce(Vector2.up * 10 * timer);
+        
+        timer = minPower;           // reset timer
+        powerSlider.value = timer;
+    }
+}
