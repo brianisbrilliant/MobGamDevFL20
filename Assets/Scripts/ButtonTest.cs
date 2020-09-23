12e57bd516a92395ba1ca796a92d1d74868abafc
Brian Foster <brianisbrilliant@gmail.com>
2020-09-22 22:39:13 -0500

    Created TiltTest and cleaned up Assets folder.

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public enum direction {Right, Left};
+
+    public direction dir;
+
+    public Transform playerShip;
+
+    [Range(10,360)]
+    public int rotSpeed = 30;
+
+    bool rotateRight, rotateLeft;
+
+    void Update() {
+        if(rotateRight) {
+            playerShip.Rotate(0,0, -1 * rotSpeed * Time.deltaTime);
+        } else if (rotateLeft) {
+            playerShip.Rotate(0,0, 1 * rotSpeed * Time.deltaTime);
+        }
+    }
+
+    public void ButtonDown(string givenDir) {
+        print(givenDir + " button Down.");
+        if(givenDir == "Right") {
+            rotateRight = true;
+        } else if(givenDir == "Left"){
+            rotateLeft = true;
+        }
+    }
+
+    public void ButtonUp(string givenDir) {
+        print(givenDir + " button Up.");
+        if(givenDir == "Right") {
+            rotateRight = false;
+        } else if(givenDir == "Left"){
+            rotateLeft = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TiltPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TiltPlayer : MonoBehaviour
+{
+	public float speed = 10, rotSpeed = 60;
+	public TextMeshProUGUI healthText;
+
+	Transform body;
+	int health = 5;
+	bool canBeHurt = true;
+
+	void Start() {
+		body = transform.GetChild(0);
+	}
+  
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+		dir.x = -Input.acceleration.x;		// x to x
+		dir.y = -Input.acceleration.z;		// y to z
+		dir.z = 1;
+
+		// clamp acceleration vector to unit sphere
+		if(dir.sqrMagnitude > 1) {
+			dir.Normalize();
+		}
+
+		// make it move 10 meters per second instead of 10 meters per frame
+		dir *= Time.deltaTime;
+
+		// move object
+		transform.Translate(dir * speed);
+		body.Rotate(0,0,-dir.x * rotSpeed);
+    }
+
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("I've hit something!");
+		if(canBeHurt){
+			Debug.Log("I've been hurt! Health: " + health);
+			health -= 1;
+			healthText.text = "Health: " + health;
+			if(health <= 0) {
+				Application.LoadLevel(4);
+			}
+			Debug.Log("I've been hurt! Health: " + health);
+			StartCoroutine(Invulnerable());
+		}
+	}
+
+	IEnumerator Invulnerable() {
+		canBeHurt = false;
+		yield return new WaitForSeconds(0.5f);
+		canBeHurt = true;
+	}
+}
